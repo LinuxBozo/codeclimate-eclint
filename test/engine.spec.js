3335e6b05f8c5714935c55c072add9f7c008f679
M. Adam Kendall <michael.kendall@gsa.gov>
2017-05-06 11:26:58 -0400

    Do some refactoring, add more tests, produce fingerprint

--- test/engine.spec.js
+++ test/engine.spec.js
@@ -15,6 +15,11 @@ describe('Engine', () => {
   });
 
   describe('#isFileExcluded', () => {
+    it('should exclude nodejs dependencies', (done) => {
+      const testpath = '/myproject/node_modules/foo';
+      assert.ok(engine.isFileExcluded(testpath));
+      done();
+    });
     it('should exclude vendored dependencies', (done) => {
       const testpath = '/myproject/vendor/foo';
       assert.ok(engine.isFileExcluded(testpath));
@@ -41,7 +46,7 @@ describe('Engine', () => {
   });
 
   describe('#prunePathsWithinSymlinks', () => {
-    it('should exclude symlink paths', (done) => {
+    it('should only exclude symlink paths', (done) => {
       const paths = glob.sync(path.join(__dirname,'/fixtures/**/*'));
       const globExpected = [
         path.join(__dirname, '/fixtures/binary.dat'),
@@ -61,11 +66,36 @@ describe('Engine', () => {
   });
 
   describe('#inclusionBasedFileListBuilder', () => {
-    it('should only contain a single path from fixtures', (done) => {
+    it('should only contain a single path from whole fixtures path', (done) => {
       const expected = [path.join(__dirname, '/fixtures/test.js')];
       const includedPaths = engine.inclusionBasedFileListBuilder();
       assert.deepEqual(includedPaths, expected);
       done();
+    });
+    it('should contain no files when fed only symlink', () => {
+      config = {
+        include_paths: [path.join(__dirname, '/fixtures/test_symlink.js')]
+      };
+      engine = new Engine(config);
+      const includedPaths = engine.inclusionBasedFileListBuilder();
+      assert.deepEqual(includedPaths, []);
+    });
+    it('should contain no files when fed only binary file', () => {
+      config = {
+        include_paths: [path.join(__dirname, '/fixtures/binary.dat')]
+      };
+      engine = new Engine(config);
+      const includedPaths = engine.inclusionBasedFileListBuilder();
+      assert.deepEqual(includedPaths, []);
+    });
+    it('should contain one file when fed javascript file', () => {
+      config = {
+        include_paths: [path.join(__dirname, '/fixtures/test.js')]
+      };
+      engine = new Engine(config);
+      const expected = [path.join(__dirname, '/fixtures/test.js')];
+      const includedPaths = engine.inclusionBasedFileListBuilder();
+      assert.deepEqual(includedPaths, expected);
     });
   });
 
@@ -75,6 +105,16 @@ describe('Engine', () => {
       console.log = function (message) {
         let content = JSON.parse(message.replace(/\0/g, ''));
         assert.equal(content.check_name, 'indent_size');
+        console.log = oldLog;
+        done();
+      };
+      engine.run();
+    });
+    it('should return a single issue and have an unique fingerprint', (done) => {
+      const oldLog = console.log;
+      console.log = function (message) {
+        let content = JSON.parse(message.replace(/\0/g, ''));
+        assert.equal(content.fingerprint, '23ca1cd56617e221a207d41e46a07777f1a8f3e31c73aafcba0a1d4fe63e9e70');
         console.log = oldLog;
         done();
       };
