f1656bc4621827a37c6d0d8b24acfc4a67aae1a5
M. Adam Kendall <michael.kendall@gsa.gov>
2017-05-08 21:51:08 -0400

    Refactor with promises for batching

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ IMAGE_NAME ?= codeclimate/codeclimate-eclint
 
 image:
 	docker build --rm -t $(IMAGE_NAME) .
+
+test: image
+	docker run --rm -v $$PWD/test/fixtures:/code $(IMAGE_NAME) sh -c "cd /usr/src/app && npm test"
--- test/engine.spec.js
+++ test/engine.spec.js
@@ -6,12 +6,17 @@ const Engine = require('../lib/engine');
 describe('Engine', () => {
   let config;
   let engine;
+  let iobuff = [];
+
+  const logger = (message) => {
+    iobuff.push(message);
+  };
 
   beforeEach(() => {
     config = {
       include_paths: [path.join(__dirname, '/fixtures')]
     };
-    engine = new Engine(config);
+    engine = new Engine(config, logger);
   });
 
   describe('#isFileExcluded', () => {
@@ -51,6 +56,7 @@ describe('Engine', () => {
       const globExpected = [
         path.join(__dirname, '/fixtures/binary.dat'),
         path.join(__dirname, '/fixtures/test_indent_size.js'),
+        path.join(__dirname, '/fixtures/test_multiple_issues.js'),
         path.join(__dirname, '/fixtures/test_symlink.js'),
         path.join(__dirname, '/fixtures/test.js')
       ];
@@ -59,6 +65,7 @@ describe('Engine', () => {
       const expected = [
         path.join(__dirname, '/fixtures/binary.dat'),
         path.join(__dirname, '/fixtures/test_indent_size.js'),
+        path.join(__dirname, '/fixtures/test_multiple_issues.js'),
         path.join(__dirname, '/fixtures/test.js')
       ];
       const nonSymlinks = engine.prunePathsWithinSymlinks(paths);
@@ -71,6 +78,7 @@ describe('Engine', () => {
     it('should only contain a two files from whole fixtures path', (done) => {
       const expected = [
         path.join(__dirname, '/fixtures/test_indent_size.js'),
+        path.join(__dirname, '/fixtures/test_multiple_issues.js'),
         path.join(__dirname, '/fixtures/test.js')
       ];
       const includedPaths = engine.inclusionBasedFileListBuilder();
@@ -81,7 +89,7 @@ describe('Engine', () => {
       config = {
         include_paths: [path.join(__dirname, '/fixtures/test_symlink.js')]
       };
-      engine = new Engine(config);
+      engine = new Engine(config, logger);
       const includedPaths = engine.inclusionBasedFileListBuilder();
       assert.deepEqual(includedPaths, []);
     });
@@ -89,7 +97,7 @@ describe('Engine', () => {
       config = {
         include_paths: [path.join(__dirname, '/fixtures/binary.dat')]
       };
-      engine = new Engine(config);
+      engine = new Engine(config, logger);
       const includedPaths = engine.inclusionBasedFileListBuilder();
       assert.deepEqual(includedPaths, []);
     });
@@ -97,7 +105,7 @@ describe('Engine', () => {
       config = {
         include_paths: [path.join(__dirname, '/fixtures/test.js')]
       };
-      engine = new Engine(config);
+      engine = new Engine(config, logger);
       const expected = [path.join(__dirname, '/fixtures/test.js')];
       const includedPaths = engine.inclusionBasedFileListBuilder();
       assert.deepEqual(includedPaths, expected);
@@ -105,43 +113,57 @@ describe('Engine', () => {
   });
 
   describe('#run', () => {
-    it('should return a single issue for indent_size', (done) => {
-      const oldLog = console.log;
-      console.log = function (message) {
-        let content = JSON.parse(message.replace(/\0/g, ''));
-        assert.equal(content.check_name, 'indent_size');
-        console.log = oldLog;
-        done();
-      };
-      engine.run();
+    beforeEach(() => {
+      iobuff = [];
     });
-    it('should return a single issue and have an unique fingerprint', (done) => {
-      const oldLog = console.log;
-      console.log = function (message) {
-        let content = JSON.parse(message.replace(/\0/g, ''));
-        assert.equal(content.fingerprint, '20330fefe3c979047d8f672f69d3209dc89fb8384ffb649717700ec253af80a4');
-        console.log = oldLog;
-        done();
-      };
-      engine.run();
-    });
-    it('should return a no issue when no valid files included', (done) => {
+    it('should return a single issue and have fingerprint', (done) => {
       config = {
-        include_paths: [path.join(__dirname, '/fixtures/binary.dat')]
+        include_paths: [path.join(__dirname, '/fixtures/test_indent_size.js')]
       };
-      engine = new Engine(config);
-      engine.run();
-      assert(!console.log.isCalled);
-      done();
+      engine = new Engine(config, logger);
+      engine.run().then(() => {
+        assert.equal(iobuff.length, 1);
+        let issue = JSON.parse(iobuff[0].replace(/\0/g, ''));
+        assert.equal(issue.check_name, 'indent_size');
+        assert.equal(issue.fingerprint.length, 64);
+        done();
+      });
     });
-    it('should return a no issue when all files are valid', (done) => {
+    it('should return multiple issues', (done) => {
+      config = {
+        include_paths: [path.join(__dirname, '/fixtures/test_multiple_issues.js')]
+      };
+      engine = new Engine(config, logger);
+      engine.run().then(() => {
+        assert.equal(iobuff.length, 2);
+        let issue0 = JSON.parse(iobuff[0].replace(/\0/g, ''));
+        let issue1 = JSON.parse(iobuff[1].replace(/\0/g, ''));
+        assert.equal(issue0.check_name, 'trim_trailing_whitespace');
+        assert.equal(issue1.check_name, 'indent_size');
+        done();
+      });
+    });
+    it('should return no issue when all files are valid', (done) => {
       config = {
         include_paths: [path.join(__dirname, '/fixtures/test.js')]
       };
-      engine = new Engine(config);
-      engine.run();
-      assert(!console.log.isCalled);
-      done();
+      engine = new Engine(config, logger);
+      engine.run().then(() => {
+        assert(!logger.isCalled);
+        assert.equal(iobuff.length, 0);
+        done();
+      });
+    });
+    it('should return no issue when there are no valid files to analyze', (done) => {
+      config = {
+        include_paths: [path.join(__dirname, '/fixtures/binary.dat')]
+      };
+      engine = new Engine(config, logger);
+      engine.run().then(() => {
+        assert(!logger.isCalled);
+        assert.equal(iobuff.length, 0);
+        done();
+      });
     });
   });
 
